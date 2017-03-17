31ce5b4903d1dff27f91a45605b09bc7086f939f
Dianna Faulk <diannafaulk@gmail.com>
2017-03-17 14:59:40 -0700

    refactor: move app config to separate file

---
+++ app/app.config.js
@@ -0,0 +1,11 @@
+(function() {
+  'use strict';
+
+  angular.module('myApp')
+
+  .config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
+    $locationProvider.hashPrefix('!');
+
+    $routeProvider.otherwise({redirectTo: '/view1'});
+  }]);
+})();
--- app/app.js
+++ app/app.js
@@ -7,10 +7,5 @@
     'myApp.view1',
     'myApp.view2',
     'myApp.version'
-  ]).
-  config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
-    $locationProvider.hashPrefix('!');
-
-    $routeProvider.otherwise({redirectTo: '/view1'});
-  }]);
+  ]);
 })();
