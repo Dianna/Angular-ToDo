3b08def12044918440f9b671fd91250b66038a69
Dianna Faulk <diannafaulk@gmail.com>
2017-03-18 12:38:50 -0700

    chore: update active nav pill on route change

--- app/app.js
+++ app/app.js
@@ -4,9 +4,12 @@
   // Declare app level module which depends on views, and components
   angular.module('myApp', [
     'ngRoute',
+    'ui.bootstrap',
+    // Routes
     'myApp.active',
     'myApp.complete',
-    'myApp.version',
-    'ui.bootstrap'
+    // Directives
+    'myApp.activeLink',
+    'myApp.version'
   ]);
 })();
