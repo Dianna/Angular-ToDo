806632251d0cfc4a940ace559fb9963eef3f36b2
Dianna Faulk <diannafaulk@gmail.com>
2017-03-19 19:26:59 -0700

    chore: update vm.allTasks on service data updates

--- app/routes/allTasks/allTasks.controller.js
+++ app/routes/allTasks/allTasks.controller.js
@@ -1,9 +1,19 @@
 (function() {
   'use strict';
 
-  angular.module('myApp.allTasks')
+  angular
+  .module('myApp.allTasks')
+  .controller('AllTasksCtrl', AllTasksCtrl);
 
-  .controller('AllTasksCtrl', [function() {
-    
-  }]);
+  AllTasksCtrl.$inject = ['$scope', 'task'];
+
+  function AllTasksCtrl($scope, task) {
+    var vm = this;
+
+    $scope.$watch(function() {
+      return task.cache;
+    }, function (newCache, oldCache) {
+      vm.allTasks = newCache;
+    }, true);
+  }
 })();
