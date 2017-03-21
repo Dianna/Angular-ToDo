cb46adf682a87095157019a19e5745f9f1682cd9
Dianna Faulk <diannafaulk@gmail.com>
2017-03-20 17:05:43 -0700

    chore: update vm.activeTasks on service data updates

--- app/routes/active/active.controller.js
+++ app/routes/active/active.controller.js
@@ -1,9 +1,25 @@
 (function() {
   'use strict';
 
-  angular.module('myApp.active')
+  angular
+  .module('myApp.active')
+  .controller('ActiveCtrl', ActiveCtrl);
 
-  .controller('ActiveCtrl', [function() {
-    
-  }]);
+  ActiveCtrl.$inject = ['$scope', 'task'];
+
+  function ActiveCtrl($scope, task) {
+    var vm = this;
+
+    vm.activeTasks = {};
+
+    $scope.$watch(function() {
+      return task.cache;
+    }, function (newCache, oldCache) {
+      for(var key in newCache) {
+        if (newCache[key].active) {
+          vm.activeTasks[key] = newCache[key];
+        }
+      }
+    }, true);
+  }
 })();
